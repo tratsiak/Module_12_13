5e7e7b514b4d63967637533578025eac2226e87d
Vlad Tretyak <uladzislau.tratsiak@gmail.com>
2025-04-16 11:01:58 +0300

    Add follow target component

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _offset;
+
+    private void LateUpdate()
+    {
+        transform.position = _target.position + _offset;
+    }
+}
